8fd00c02732af0b85cc10703e409a3c0db9e1634
Manish kalwani <kalwanimanish777@gmail.com>
2025-09-16 00:45:49 +0530

    fix: Correct sender field type in notification schema and ensure proper formatting

--- backened/src/models/notification.model.js
+++ backened/src/models/notification.model.js
@@ -1,6 +1,23 @@
 import {mongoose,Schema} from "mongoose"
 
-
 const notificationSchema=new Schema({
+    recipient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+    sender:{
+        type:"mongoose.Schema.Types.ObjectId",
+        ref:"User"
+    },
+
+    read:{
+        type:Boolean,
+        default:false
+    },
+    video:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+    }
     
-})
+},{timestamps:true})
