91ef0d56485fda35322d2ef3fa6146e937e1e150
Manish kalwani <kalwanimanish777@gmail.com>
2025-10-16 15:48:46 +0530

    feat: implement createReplyOnComment function for handling replies to comments

--- backened/src/controllers/reply.controller.js
+++ backened/src/controllers/reply.controller.js
@@ -1,4 +1,23 @@
 import { ApiResponse } from "../utils/Apiresponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import { Reply } from "../models/reply.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 
+const createReplyOnComment=asyncHandler(async(req,res)=>{
+    const {content} = req.body;
+    const {commentId} =req.params;
+    if(!req.user || !req.user?._id){
+        throw new ApiError(404,"User not authenticated");
+    }
+
+    const repl = await Reply.create({
+        comment : commentId,
+        owner : req.user._id,
+        content : content,
+    });
+
+    if(!repl) throw new ApiError(401,"Error while creating a reply");
+    
+    
+})
